32f83bcc4f86f32b791bbc32e8ea1937833e1167
Subin Song <lemonshushu@lemonshushu.me>
2023-04-02 22:05:19 +0900

    초안 완성 (실행은 안해봄)

--- include/linux/rotation.h
+++ include/linux/rotation.h
@@ -1,32 +1,22 @@
 #include <linux/list.h>
 #include <linux/wait.h>
+#include <linux/mutex.h>
 
-/*
- * Readers and writers.
- *
- */
 #define ROT_READ 0
 #define ROT_WRITE 1
 
-struct access_request {
-	pid_t pid; // Process ID of requesting process
-	int low; // low degree of range
-	int high; // high degree of range
-	int lock_type; // ROT_READ or ROT_WRITE
-	wait_queue_entry_t wait_entry;
-};
-
-struct rotation_lock {
+struct lock_info {
     long id; // ID of lock
     pid_t pid; // Process ID of process that owns the lock
     int low; // low degree of range
     int high; // high degree of range
-    int lock_type; // ROT_READ or ROT_WRITE
+    int type; // ROT_READ or ROT_WRITE
     struct list_head list;
 };
 
-/* degree_to_locks: 효율성을 위해서 도입했으나 일단 지금은 신경쓰기 귀찮으므로, 1차로 구현 끝내고 나서 다시 넣기 */
-// struct degree_to_locks_entry {
-//     struct rotation_lock *lock;
-//     struct list_head list;
-// };
+struct reader_writer_lock {
+    int active_readers; // Number of active readers
+    int active_writers; // Number of active writers
+    int waiting_writers; // Number of waiting writers
+    // TODO: Information structs about other waiting readers and writers
+};
--- kernel/rotation.c
+++ kernel/rotation.c
@@ -2,7 +2,9 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 #include <linux/mutex.h>
+#include <linux/semaphore.h>
 #include <linux/wait.h>
+#include <linux/sched.h>
 
 static int device_orientation = 0;
 static DEFINE_MUTEX(orientation_lock);
@@ -22,43 +24,22 @@ SYSCALL_DEFINE1(set_orientation, int, degree)
 	return 0;
 }
 
-static LIST_HEAD(locks); // List of currently held locks
-static DEFINE_MUTEX(locks_lock); // Mutex to protect `locks`
+static LIST_HEAD(locks_info); // Information of currently held locks
+static struct reader_writer_lock locks[360];
+static DEFINE_MUTEX(locks_lock); // Mutex to protect `locks` and `locks_info`
+static int locks_initialized = 0; // Whether the locks have been initialized
 
-/* degree_to_locks: 효율성을 위해서 도입했으나 일단 지금은 신경쓰기 귀찮으므로, 1차로 구현 끝내고 나서 다시 넣기 */
-// static struct list_head degree_to_locks[360]; // List of locks for each degree
-// for (int i = 0; i < 360; i++) INIT_LIST_HEAD(&degree_to_locks[i]);
-// static DEFINE_MUTEX(degree_to_locks_lock); // Mutex to protect `degree_to_locks`
+/// @brief Initialize `locks` if they haven't yet.
+void locks_init(void);
 
-static wait_queue_head_t requests; // Wait queue for blocking requests
-init_waitqueue_head(&requests);
+static DECLARE_WAIT_QUEUE_HEAD(requests); // Queue of requests
 
 static long next_lock_id = 0; // The next lock ID to use
-
-/// @brief Add a request to the list of requests.
-/// @param[in] request The request to add.
-/// @param[out] lock_id The ID of the lock associated with the request.
-void add_request(struct access_request *request);
-
-/// @brief Remove a request.
-/// @param request The request to remove.
-void remove_request(struct access_request *request);
-
-/// @brief Find a request by process ID.
-/// @param pid The process ID to search for.
-/// @return The request, or NULL if not found.
-struct access_request *find_request(pid_t pid);
-
-/// @brief Filter the list of requests by degree and lock type.
-/// @param degree The degree.
-/// @param lock_type The lock type.
-/// @return The filtered list.
-struct list_head *filter_requests(int degree, int lock_type);
 
 /// @brief Find a lock by ID.
 /// @param id The ID to search for.
 /// @return The lock, or NULL if not found.
-struct rotation_lock *find_lock(long id);
+struct lock_info *find_lock(long id);
 
 /// @brief Check if current device orientation is in the specified degree range.
 /// @param low
@@ -66,10 +47,9 @@ struct rotation_lock *find_lock(long id);
 /// @return 1 if yes, 0 if no.
 int orientation_in_range(int low, int high);
 
-/// @brief Check if a lock is available.
-/// @param request The request to check.
+/// @brief Check if conditions meet to acquire a lock.
 /// @return 1 if yes, 0 if no.
-int lock_available(struct access_request *request)
+int lock_available(int low, int high, int type);
 
 /// @brief Claim read or write access in the specified degree range.
 /// @param low The beginning of the degree range (inclusive). Value must be in the range 0 <= low < 360.
@@ -78,36 +58,71 @@ int lock_available(struct access_request *request)
 /// @return On success, returns a non-negative lock ID that is unique for each call to rotation_lock. On invalid argument, returns -EINVAL.
 SYSCALL_DEFINE3(rotation_lock, int, low, int, high, int, type)
 {
+	/* Make sure the locks are initialized */
+	locks_init();
+
 	if (low < 0 || low >= 360 || high < 0 || high >= 360 ||
 	    (type != ROT_READ && type != ROT_WRITE))
 		return -EINVAL;
 
-	long id = 0; // The ID of the lock (return value)
+	/* Create a new lock */
+	struct lock_info *lock = kmalloc(sizeof(struct lock_info), GFP_KERNEL);
+	lock->id = next_lock_id++;
+	lock->pid = current->pid;
+	lock->low = low;
+	lock->high = high;
+	lock->type = type;
 
-	struct access_request *request =
-		kmalloc(sizeof(struct access_request), GFP_KERNEL);
-	if (request == NULL)
-		return -ENOMEM;
+	DEFINE_WAIT(wait);
+	add_wait_queue(requests, &wait);
+	int writer_waiting = 0; // Variable to keep track of whether `waiting_writers` is containing current process
 
-	request->pid = current->pid;
-	request->low = low;
-	request->high = high;
-	request->lock_type = type;
+	mutex_lock(&orientation_lock);
+	mutex_lock(&locks_lock);
+	while (!lock_available(low, high, type)) {
+		mutex_unlock(&orientation_lock);
+		mutex_unlock(&locks_lock);
+		prepare_to_wait(requests, &wait, TASK_INTERRUPTIBLE);
 
-	init_waitqueue_entry(&request->wait_entry, current);
-
-	add_wait_queue(&requests, &request->wait_entry);
-	while (!lock_available(request)) {
-		prepare_to_wait(&requests, &request->wait_entry, TASK_INTERRUPTIBLE);
+		/* Increment the waiting_writers count */
+		if (type == ROT_WRITE && !writer_waiting) {
+			mutex_lock(&locks_lock);
+			for (int i = low; i <= high; i++) {
+				locks[i].waiting_writers++;
+			}
+			writer_waiting = 1;
+			mutex_unlock(&locks_lock);
+		}
 		schedule();
 	}
-	// Be careful for races!!!!!!! (나중에 다시 확인)
-	// 여러 프로세스들이 기다리던 중에 동시에 lock_available이 되었을때 동시에 여기로 빠져나올수 있지만 실제로는 한 프로세스에게만 락을 줘야함.
-	finish_wait(&requests, &request->wait_entry);
+	finish_wait(requests, &wait);
+	mutex_unlock(&orientation_lock);
 
+	/* Add the lock to the list */
+	list_add(&lock->list, &locks_info);
 
-	/* Give lock to the process */
-	// TODO
+	/* Decrement the waiting_writers count */
+	if (writer_waiting) {
+		mutex_lock(&locks_lock);
+		for (int i = low; i <= high; i++) {
+			locks[i].waiting_writers--;
+		}
+		writer_waiting = 0;
+		mutex_unlock(&locks_lock);
+	}
+
+	/* Increment the active_xxx count */
+	for (int i = low; i <= high; i++) {
+		if (type == ROT_READ) {
+			locks[i].active_readers++;
+		} else {
+			locks[i].active_writers++;
+		}
+	}
+
+	mutex_unlock(&locks_lock);
+
+	return lock->id;
 }
 
 /// @brief Revoke access claimed by a call to rotation_lock.
@@ -119,7 +134,7 @@ SYSCALL_DEFINE1(rotation_unlock, long, id)
 	if (id < 0)
 		return -EINVAL;
 
-	struct rotation_lock *lock = find_lock(id);
+	struct lock_info *lock = find_lock(id);
 
 	/* No such lock, return -EINVAL */
 	if (lock == NULL)
@@ -132,7 +147,19 @@ SYSCALL_DEFINE1(rotation_unlock, long, id)
 	/* Delete the lock from list */
 	mutex_lock(&locks_lock);
 	list_del(&lock->list);
+	for (int i = lock->low; i <= lock->high; i++) {
+		if (lock->type == ROT_READ) {
+			locks[i].active_readers--;
+		} else {
+			locks[i].active_writers--;
+		}
+	}
 	mutex_unlock(&locks_lock);
+
+	kfree(lock);
+
+	/* Wake up all processes waiting for the lock */
+	wake_up_all(requests);
 
 	return 0;
 }
@@ -145,8 +172,46 @@ int orientation_in_range(int low, int high)
 		return device_orientation >= low || device_orientation <= high;
 }
 
-int lock_available(struct access_request *request)
+int lock_available(int low, int high, int type)
 {
-	// TODO
-	return 0;
+	if (!orientation_in_range(low, high))
+		return 0;
+
+	if (type == ROT_READ) {
+		/* Reader */
+
+		int flag = 1; // Whether the lock is available
+		for (int i = low; i <= high; i++) {
+			if (locks[i].active_writers > 0 ||
+			    locks[i].waiting_writers > 0) {
+				flag = 0;
+				break;
+			}
+		}
+		return flag;
+	} else {
+		/* Writer */
+
+		int flag = 1; // Whether the lock is available
+		for (int i = low; i <= high; i++) {
+			if (locks[i].active_readers > 0 ||
+			    locks[i].active_writers > 0) {
+				flag = 0;
+				break;
+			}
+		}
+		return flag;
+	}
+}
+
+void locks_init(void)
+{
+	if (locks_initialized)
+		return;
+	for (int i = 0; i < 360; i++) {
+		locks[i].active_readers = 0;
+		locks[i].active_writers = 0;
+		locks[i].waiting_writers = 0;
+	}
+	locks_initialized = 1;
 }
